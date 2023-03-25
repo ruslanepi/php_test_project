71742ff0cc8c941ff91fcbe9fa664b92d9309556
ruslanepi <ruslanep2@gmail.com>
2023-03-26 02:14:20 +0300

    added form validation

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -8,7 +8,7 @@
                         @csrf
                         <div class="mb-3">
                             <label for="title" class="form-label">Title </label>
-                            <input name="title" type="text" class="form-control" id="title" placeholder="title">
+                            <input value="{{old('title')}}" name="title" type="text" class="form-control" id="title" placeholder="title">
                             @error('title')
                             <p class="text-danger">{{$message}}</p>
                             @enderror
@@ -16,17 +16,25 @@
                         <div class="mb-3">
                             <label for="content" class="form-label">Content </label>
                             <textarea name="content" type="text" class="form-control" id="content"
-                                      placeholder="content"></textarea>
+                                      placeholder="content">{{old('content')}}</textarea>
+                            @error('content')
+                            <p class="text-danger">{{$message}}</p>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label for="Image" class="form-label">Image </label>
-                            <input name="image" type="text" class="form-control" id="image" placeholder="image">
+                            <input value="{{old('image')}}" name="image" type="text" class="form-control" id="image" placeholder="image">
+                            @error('image')
+                            <p class="text-danger">{{$message}}</p>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="category">Choose category</label>
                             <select id="category" class=" form-select mb-3" name="category_id">
                                 @foreach($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->title}}</option>
+                                    <option
+                                        {{ old('category_id') == $category->id ? ' selected' : '' }}
+                                        value="{{$category->id}}">{{$category->title}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -34,7 +42,9 @@
                             <label for="category">Choose tags</label>
                             <select multiple id="tags" class=" form-select mb-3" name="tags[]">
                                 @foreach($tags as $tag)
-                                    <option value="{{$tag->id}}">{{$tag->title}}</option>
+                                    <option
+                                        {{old('tags') !=null && in_array($tag->id, old('tags')) ? 'selected' : ''}}
+                                        value="{{$tag->id}}">{{$tag->title}}</option>
                                 @endforeach
                             </select>
                         </div>
